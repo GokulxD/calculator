29c51d915db36ee00cb93a1c641102bbd9f5ce8f
Gᴏᴋᴜʟ 豈 <98200081+GokulxD@users.noreply.github.com>
2023-12-08 19:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let display = document.getElementById('display');
+let currentInput = '';
 
+function appendToDisplay(value) {
+  currentInput += value;
+  display.innerHTML = currentInput;
+}
+
+function clearDisplay() {
+  currentInput = '';
+  display.innerHTML = '0';
+}
+
+function calculate() {
+  try {
+    currentInput = eval(currentInput).toString();
+    display.innerHTML = currentInput;
+  } catch (error) {
+    display.innerHTML = 'Error';
+    setTimeout(clearDisplay, 1000);
+  }
+}
